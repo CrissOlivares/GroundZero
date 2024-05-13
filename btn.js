736a773fa2b80d8dbc290e61fdc842f861f5f5f6
Miguel Astorga <xmiguelastorgax@gmail.com>
2024-05-13 14:25:51 -0400

    nuevos comentario

--- btn.js
+++ btn.js
@@ -1,3 +1,4 @@
+//getElementById=obtener elemento de aqui
 const botonPago = document.getElementById('botonPago');
 const boton1 = document.getElementById('boton1');
 const boton2 = document.getElementById('boton2');
@@ -7,9 +8,8 @@ const textoMostrado2 = document.getElementById('textoMostrado2');
 const numeroFijo = 10; // Número fijo que deseas mostrar en el texto
 const numer = 900;
 
-function actualizarTextoPago(total) {
-  botonPago.textContent = "Pagar = $" + total;
-}
+//addEventListener= le agrego un evento
+
 
 boton1.addEventListener('click', function() {
   const total = numer + numeroFijo;
@@ -24,3 +24,7 @@ boton2.addEventListener('click', function() {
   textoMostrado2.textContent = "Total: $" + numer;
   actualizarTextoPago(total);
 });
+
+function actualizarTextoPago(total) {
+  botonPago.textContent = "Pagar = $" + total;
+}
